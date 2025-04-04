85e83ca9bbfadb205c88596bdf32a8f592e5f3b6
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2025-04-03 21:24:31 -0600

    pleease bro

--- backend/Mission11Ass/Mission11Ass/obj/Debug/net8.0/Mission11Ass.AssemblyInfo.cs
+++ backend/Mission11Ass/Mission11Ass/obj/Debug/net8.0/Mission11Ass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11Ass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0a450d5efda6887a4fc3e22f4df51550dbd57f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7895741cd0bae846c870e7f0cb997f3980765536")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11Ass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11Ass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter({
     const fetchCategories = async () => {
       try {
         const response = await fetch(
-          'https://localhost:9999/Book/GetCategories'
+          'https://bookstore-z-back-fgfgcxcafrhwd0fy.eastus-01.azurewebsites.net/Book/GetCategories'
         );
         const data = await response.json();
         console.log('Fetched categories', data);
